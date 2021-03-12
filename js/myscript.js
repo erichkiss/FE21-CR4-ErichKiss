5cb42328aee780fd37608d98cd065f384f4c8a4a
Erich Kiss <erich.kiss2@gmail.com>
2021-03-12 14:02:00 +0100

    with comments

--- js/myscript.js
+++ js/myscript.js
@@ -1,13 +1,7 @@
 let movies = JSON.parse(movieDB);
-var j = 0;
 var outputID = 0;
 
-// console.log(movies);
-// movies[0].likes++;
-// console.log(movies[0].likes);
-
-
-for (let i of movies) {
+for (let i of movies) { // in this loop I print all Bootstrap cards
     document.getElementById("viewCards").innerHTML += ` 
                                                     <div id="movies_div" class="card m-1 col-lg-6 col-md-6 " style="max-width: 540px;">
                                                         <div class="row no-gutters">
@@ -20,34 +14,38 @@ for (let i of movies) {
                                                                 <h6 class="card-title">${i.year}</h6>
                                                                 <p class="card-text">${i.description}</p>
                                                                 <p id="${outputID}"class="card-text">Likes ${i.likes}</p>
-                                                                <a id="${i.Id}" class="btn btn-primary">Like</a>
+                                                                <a id="${i.Id}" class="btn btn-primary">Like</a> 
                                                             </div>
                                                             </div>
                                                         </div>
                                                     </div>
                                                     `;
 
-    // console.log(i.Id);
-    outputID++;
-    // console.log("ID" + outputID);
+    //as you'll see below, I added 
+    
+    outputID++; // I'm sure there is a more elegant way to adress the correct output element- but to proceed to the increment logic, I leave here
+    // currently the confusion in my head makes it hard to remember the correct methods
+    
+  
 };
 
-function outputLikes (n) {
-    movies[n].likes++;
-    for (l = 0; l < movies.length; l++) {
-        document.getElementById(l).innerHTML = "Likes " + movies[l].likes;
-    }
+function outputLikes (n) { // In case of a click, I increment movies[n].likes and output the line at n => outputID element
+    movies[n].likes++;     
+    document.getElementById(n).innerHTML = "Likes " + movies[n].likes;  
 };
 
-for (let i in movies) {
+for (let i in movies) { // adding eventListener to all buttons. To distinguish between the buttons, I assigned the movies.Id as the button element ID ;)
     document.getElementById(movies[i].Id).addEventListener("click", function (){outputLikes(i)});
+    // I tried to assign the EventListener in a loop where I go through an array of getElementsByClassName items - but I always ended 
+    // up with an error ".addEventListener is not a function"
+
 };
 
 
 
 
 
-// Vertical Card 
+// Vertical Card in Bootstrap
 // <div id="main_div" class="col-lg-4 col-md-6 ${i.Id}">
 //         <div class="card">
 //         <img src="${i.image}" class="card-img-top" alt="...">
